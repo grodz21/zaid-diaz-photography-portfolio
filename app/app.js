d8bc000485f197509a9df7055c67928d5d41e244
Gabriel <gaby4b11@gmail.com>
2017-06-06 23:44:14 -0400

    jQuery editing
    Added a quicker way of fixing the sub-title in all pages at once
    instead of going one by one on each page.

--- app/app.js
+++ app/app.js
@@ -2,3 +2,6 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 	    event.preventDefault();
 	    $(this).ekkoLightbox();	
 });
+
+//Replaces sub-title below the author's website h1 name
+$( "#aboutsite" ).replaceWith( '<div class="col-12" id="aboutsite">' + "Photographer | TEST | Daydreamer" + '</div>' );
